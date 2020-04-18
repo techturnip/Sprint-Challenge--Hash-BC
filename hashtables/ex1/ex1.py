7410bb1b0bdf3bc78ada3e58bce21c85c7810c5f
Tyler Turnipseed <tyler@techturnip.us>
2020-04-18 12:46:46 -0400

    solution for ex1 in hashtables

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,12 +9,33 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
+    print("\nRUN")
+
     # print the inputs we are working with
-    print(f'\nLength: {length}')
-    print(f'Weights: {weights}')
+    # print(f'\nLength: {length}')
+    # print(f'Weights: {weights}')
     print(f'Limit: {limit}')
 
-    return None
+    # loop through weights array
+    for i in range(0, length):
+        # store the weight = weights[i]
+        weight = weights[i]
+        print(f'Weight: {weight}')
+
+        # store the difference between the weight and limit
+        difference = limit - weight
+        print(f'Difference: {difference}')
+
+        # check if hash table contains an entry for limit - weight
+        retrieved = hash_table_retrieve(ht, difference)
+        print(retrieved)
+
+        if retrieved is not None:
+            print((i, retrieved))
+            return (i, retrieved)
+
+        # store weight's list index as value
+        hash_table_insert(ht, weight, i)
 
 
 def print_answer(answer):
